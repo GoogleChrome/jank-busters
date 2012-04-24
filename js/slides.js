8c3dc9b8a3d03f23f92ce743a16062ebb75f8b58
Eric Bidelman <ebidel@gmail.com>
2012-04-24 10:27:36 -0700

    Making local dev work in all cases for presenter mode

--- js/slides.js
+++ js/slides.js
@@ -3,7 +3,7 @@
  */
 (function() {
   var scs = ['modernizr.custom.45394.js', 'prettify/prettify.js', 'hammer.js',
-      'slide-controller.js', 'slide-deck.js'];
+             'slide-controller.js', 'slide-deck.js'];
   var b = document.getElementsByTagName('script')[0];
   for (var i = 0, s; s = scs[i]; i++) {
     var sc = document.createElement('script');
