a4bca99bc19548fa31a26aa7713579df8570a0e2
Luke <lukem@google.com>
2012-06-04 16:37:20 +1000

    handling https fonts

--- js/slide-deck.js
+++ js/slide-deck.js
@@ -408,7 +408,8 @@ SlideDeck.prototype.loadConfig_ = function(config) {
 SlideDeck.prototype.addFonts_ = function(fonts) {
   var el = document.createElement('link');
   el.rel = 'stylesheet';
-  el.href = 'http://fonts.googleapis.com/css?family=' + fonts.join('|') + '&v2';
+  e.href = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://fonts.googleapis.com/css?family=' + fonts.join('|') + '&v2';
   document.querySelector('head').appendChild(el);
 };
 
