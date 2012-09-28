de6933fc80953ea749d22b52b08f84a1b4d6da02
Tom Wiltzius <tom.wiltzius@gmail.com>
2012-09-27 23:03:02 -0700

    checking in velocity europe slides

---
+++ velocity-europe/slide_config.js
@@ -0,0 +1,29 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Jank-Free',
+    subtitle: 'In Pursuit of Smooth Web Apps',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1',
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [
+  {
+    name: 'Tom Wiltzius',
+    company: 'Product Manager, Chrome Graphics, Google',
+    email: 'wiltzius@chromium.org',
+    www: 'http://www.jankfree.com',
+    github: 'http://github.com/wiltzius'
+  }]
+};
+
